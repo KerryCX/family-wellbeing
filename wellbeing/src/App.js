2a725b0b047d75227d18cf96f73a85b163e8e701
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-15 12:33:03 +0000

    setting up JSON

--- wellbeing/src/App.js
+++ wellbeing/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("carbs.JSON")
+      const data = await response.json()
+      console.log({ data })
+    }
+    fetchData()
+  }, [])
+
+    return (
+    <div>
+      <dl>
+        <dt><b>Carbs</b></dt>
+        <dd>Bread</dd>
+        <dd>Pasta</dd>
+      </dl>
     </div>
   );
 }
